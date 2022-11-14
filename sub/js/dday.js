8bfe7c9c824b8edc575d170d83746ba2d8387fb5
shinhee <tlsgml656@gmail.com>
2022-11-14 21:10:24 +0900

    d-day 페이지 작업

---
+++ sub/js/dday.js
@@ -0,0 +1,33 @@
+const $ddayTime = document.querySelector("h2");
+
+const getDDay = () => {
+	// D-Day 날짜 지정
+	const setDate = new Date("2023-11-16");
+	const setDateYear = setDate.getFullYear();
+	const setDateMonth = setDate.getMonth() + 1;
+	const setDateDay = setDate.getDate();
+
+	const now = new Date();
+
+	const distance = setDate.getTime() - now.getTime();
+
+	const day = Math.floor(distance / (1000 * 60 * 60 * 24));
+	const hours = Math.floor(
+		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+	);
+	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+	$ddayTime.innerText = `${setDateYear}년 ${setDateMonth}월 ${setDateDay}일까지 
+      ${day}일 
+      ${hours < 10 ? `0${hours}` : hours}시간 
+      ${minutes < 10 ? `0${minutes}` : minutes}분 
+      ${seconds < 10 ? `0${seconds}` : seconds}초 남았습니다.`;
+};
+
+const init = () => {
+	getDDay();
+	setInterval(getDDay, 1000);
+};
+
+init();
