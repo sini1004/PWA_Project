03cf322e2fe453ec0ceb5d2f962bc9a71d614628
shinhee <tlsgml656@gmail.com>
2022-11-23 22:20:29 +0900

    todo 오류로 인한 전체 코드 수정

--- sub/js/todo.js
+++ sub/js/todo.js
@@ -1,58 +1,60 @@
-const toDoForm = document.getElementById('todo_form');
-const toDoInput = document.querySelector('input');
-const toDoList = document.getElementById('todo_list');
+const $toDoForm = document.getElementById("todo_form");
+const $toDoInput = $toDoForm.querySelector("input");
+const $toDoList = document.getElementById("todo_list");
 
-const TODOS_KEY = 'todos'
-const toDos = [];
+const TODOS_KEY = "todosKey"
+
+let toDos = [];
 
 function saveToDos(){
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
-function deleteTodo(event){
+
+
+function deleteToDo(event){
   const li = event.target.parentElement;
   li.remove();
-  toDos = toDos.fill((toDo)=> toDo.id !== parseInt(li.id));
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); //클릭한 li.id와 다른 toDo 남겨두기 (parseInt:문자열을 숫자로 변환)
   saveToDos();
 }
 
-function paintToDo(newTodo) {
-  const li = document.createElement('li');
+
+function paintToDo(newTodo){
+  const li = document.createElement("li");
   li.id = newTodo.id;
-  const todospan = document.createElement('span');
-  todospan.innerText = newTodo.text;
 
-  const button = document.createElement('button');
-  button.innerText = '❌';
-  button.addEventListener('click', deleteTodo);
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
 
-  li.appendChild(todospan);
+  const button = document.createElement("button");
+  button.innerText = "❌";
+
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
   li.appendChild(button);
-  toDoList.appendChild(li);
+  $toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event){
-  event.preventDefault();
-  const newTodo = toDoInput.value;
-  toDoInput.value = '';
-  const newTodoObj = {
-    text : newTodo,
-    id : Date.now()
-  };
-  toDos.push(newTodoObj);
-  paintToDo(newTodoObj);
-  saveToDos();
+	event.preventDefault();
+	const newTodo = $toDoInput.value;
+	$toDoInput.value = "";
+    const newTodoObj = {
+      text: newTodo,
+      id: Date.now()
+    }
+    toDos.push(newTodoObj);
+	paintToDo(newTodoObj);
+	saveToDos();
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit);
+$toDoForm.addEventListener("submit", handleToDoSubmit);
+
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if(savedToDos !== null){
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
-  parsedToDos.forEach (paintToDo);
-}
-
-function sexyFilter(){
-
+  parsedToDos.forEach(paintToDo);
 }
