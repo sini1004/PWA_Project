83974c27ae6e9784525a6c922be56ee3d8d43523
shinhee <tlsgml656@gmail.com>
2022-11-28 17:51:17 +0900

    todolist 기능 완료

--- sub/js/todo_n.js
+++ sub/js/todo_n.js
@@ -1,18 +1,19 @@
 function closeIcon() {
-  return `<img src="../img/trash-bin.png" alt="trash">`;
+  return `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M6 18L18 6M6 6l12 12" />
+  </svg>`;
 }
 
 function editIcon() {
-  return `<img src="../img/pencil.png" alt="pencil">`;
-}
-{/* <svg xmlns="http://www.w3.org/2000/svg" class="edit-icon h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  return `<svg xmlns="http://www.w3.org/2000/svg" class="edit-icon h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="1"
       d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
     />
-  </svg> */}
+  </svg>`;
+}
 
 const todoForm = document.querySelector(".todo_form");
 const todoItems = document.querySelector(".todo-items");
@@ -123,7 +124,7 @@ function displayFooterIfHaveTasks() {
 function countLeftItems(totalItems = 2) {
   const leftItems = document.querySelector(".left-items");
 
-  const count = totalItems > 1 ? `${totalItems} Items` : `${totalItems} Item`;
+  const count = totalItems > 1 ? `${totalItems} Items Left` : `${totalItems} Item Left`;
   leftItems.innerHTML = count;
 }
 
